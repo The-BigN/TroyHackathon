ddb00d97e9012521b258357795328e40f6b36214
The-BigN <117114526+The-BigN@users.noreply.github.com>
2023-11-04 23:11:45 -0500

    Update TroyHack.java
    Made the selection more robust and included parameters for the function for ease of integration

--- src/main/java/com/example/nutritioncalculator3/TroyHack.java
+++ src/main/java/com/example/nutritioncalculator3/TroyHack.java
@@ -1,26 +1,79 @@
 package Users.noahl.IdeaProjects.troyhack;
 import java.util.Scanner;
+import java.lang.String;
 
 public class TroyHack {
-    public static void main(String[] args)
+    public static void main(String []args){
+    bmi(180,72, 21, 1, "Low");
+    }
+
+    public static void bmi(float weight, float height, float age, float gender, String activitylvl)
     {
-        float weight, height, bmi, heightsquared;
+        String ActivityLevel = new String();
+        float pweight, pheight, bmi, heightsquared, p_age, bmr;
+        pweight = weight;
+        pheight = height;
+        p_age = age;
+        ActivityLevel = activitylvl;
+
         Scanner scan = new Scanner(System.in);
-        System.out.println("Input Height: ");
-        height = scan.nextInt();
-        System.out.println("Input Weight: ");
-        weight = scan.nextInt();
 
-        System.out.println("Height: " + height);
-        System.out.println("Weight: " + weight);
+        heightsquared = pheight * pheight;
 
-        heightsquared = height * height;
-
-        System.out.println("Height squared: " + heightsquared);
-
-        bmi = (weight/heightsquared) * 703;
+        bmi = (pweight/heightsquared) * 703;
 
         System.out.printf("BMI is: " + "%,.2f", bmi);
+
+        //1 is male 0 is female :)
+
+        if (gender == 1 && ActivityLevel == "Low")
+        {
+            bmr = (float) ((float) (((10 * (pweight / 2.204)) + (6.25 * pheight * 2.54) - (5 * p_age) + 5)) * 1.1);
+            System.out.printf("BMR is: " + "%,.2f", bmr);
+
+        }
+        else if (gender == 0 && ActivityLevel == "None")
+        {
+            bmr = (float) ((10 * (pweight / 2.204)) + (6.25 * pheight * 2.54) - (5 * p_age) + 5);
+            System.out.printf("BMR is: " + "%,.2f", bmr);
+
+        }
+        else if (gender == 1 && ActivityLevel == "Med")
+        {
+            bmr = (float) ((float) (((10 * (pweight / 2.204)) + (6.25 * pheight * 2.54) - (5 * p_age) + 5)) * 1.4);
+            System.out.printf("BMR is: " + "%,.2f", bmr);
+
+        }
+        else if (gender == 1 && ActivityLevel == "High")
+        {
+            bmr = (float) ((float) (((10 * (pweight / 2.204)) + (6.25 * pheight * 2.54) - (5 * p_age) + 5)) * 1.65);
+            System.out.printf("BMR is: " + "%,.2f", bmr);
+
+        }
+        if (gender == 0 && ActivityLevel == "Low")
+        {
+            bmr = (float) ((float) (((10 * (pweight / 2.204)) + (6.25 * pheight * 2.54) - (5 * p_age) - 161)) * 1.1);
+            System.out.printf("BMR is: " + "%,.2f", bmr);
+
+        }
+        else if (gender == 0 && ActivityLevel == "Med")
+        {
+            bmr = (float) ((float) (((10 * (pweight / 2.204)) + (6.25 * pheight * 2.54) - (5 * p_age) - 161)) * 1.4);
+            System.out.printf("BMR is: " + "%,.2f", bmr);
+
+        }
+        else if (gender == 0 && ActivityLevel == "High")
+        {
+            bmr = (float) ((float) (((10 * (pweight / 2.204)) + (6.25 * pheight * 2.54) - (5 * p_age) - 161)) * 1.65);
+            System.out.printf("BMR is: " + "%,.2f", bmr);
+
+        }
+        else if (gender == 0 && ActivityLevel == "None")
+        {
+            bmr = (float) ((10 * (pweight / 2.204)) + (6.25 * pheight * 2.54) - (5 * p_age) - 161);
+            System.out.printf("BMR is: " + "%,.2f", bmr);
+
+        }
 
     }
 }
